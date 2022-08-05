727db6f0886e2ee2947076e8ac7bec9c82979f17
KINPEACE <nikita_1997152@mail.ru>
2022-08-06 00:22:06 +0300

    add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите 1-ое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine( num1 + ">" + num2);
+}
+else
+{
+    Console.WriteLine( num2 + ">" + num1);
+}
+
+if(num1 < num2)
+{
+    Console.WriteLine( num1 + "<" + num2);
+}
+else
+{
+    Console.WriteLine( num2 + "<" + num1);
+}
